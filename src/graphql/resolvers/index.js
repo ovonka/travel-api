053a9e955c28110aafb92ceded1594d37072a884
Nkanyiso Ntshangase <102148695+ovonka@users.noreply.github.com>
2025-08-07 21:56:49 +0200

    bugfix: typo fix

--- src/graphql/resolvers/index.js
+++ src/graphql/resolvers/index.js
@@ -1,5 +1,5 @@
 import { cityResolvers } from './cityResolvers.js';
 import { weatherResolvers } from './weatherResolver.js';
-import { activityResolvers } from './activityResolvers.js';
+import { activityResolvers } from './activityResolver.js';
 
 export const resolvers = [cityResolvers, weatherResolvers, activityResolvers];

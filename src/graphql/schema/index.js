3cf83c5e4b502a00e8539c91f6feaaa7ab372038
Nkanyiso Ntshangase <102148695+ovonka@users.noreply.github.com>
2025-08-07 12:50:02 +0200

    feat: updated the schema root file to include cityDefs, handling this way for future implementation

--- src/graphql/schema/index.js
+++ src/graphql/schema/index.js
@@ -1,11 +1,3 @@
-import { gql } from 'graphql-tag';
 import { cityTypeDefs } from './city.js';
 
-export const typeDefs = [
-  gql`
-    type Query {
-      cities: [City]
-    }
-  `,
-  cityTypeDefs,
-];
+export const typeDefs = [cityTypeDefs];

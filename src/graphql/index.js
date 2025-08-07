5f434a25488473ed8c04d9a7a5c140d21b15f99d
Nkanyiso Ntshangase <102148695+ovonka@users.noreply.github.com>
2025-08-07 21:56:37 +0200

    refactor: modifed the utilization og resolvers , to align with the schemas as well

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -2,9 +2,7 @@ import { mergeTypeDefs } from '@graphql-tools/merge';
 import { mergeResolvers } from '@graphql-tools/merge';
 
 import { typeDefs as baseTypeDefs } from './schema/index.js';
-import { cityResolvers } from './resolvers/cityResolvers.js';
-import { weatherResolvers } from './resolvers/weatherResolver.js';
-import { activityResolvers } from './resolvers/activityResolver.js';
+import { resolvers as baseResolvers } from './resolvers/index.js';
 
 export const typeDefs = mergeTypeDefs(baseTypeDefs);
-export const resolvers = mergeResolvers([cityResolvers, weatherResolvers, activityResolvers]);
+export const resolvers = mergeResolvers(baseResolvers);

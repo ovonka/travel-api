ab2318740559f8a0d2fd067292a65b89ed4d6dab
Nkanyiso Ntshangase <102148695+ovonka@users.noreply.github.com>
2025-08-07 12:44:20 +0200

    feat: modified the dummy data i was using to test the city resolver into dynamic logic

--- src/graphql/resolvers/cityResolvers.js
+++ src/graphql/resolvers/cityResolvers.js
@@ -1,9 +1,9 @@
+import { suggestCities } from '../../services/cityService.js';
+
 export const cityResolvers = {
   Query: {
-    cities: () => [
-      { id: '1', name: 'Johannesburg', country: 'South Africa' },
-      { id: '2', name: 'Lagos', country: 'Nigeria' },
-      { id: '3', name: 'Nairobi', country: 'Kenya' },
-    ],
+    suggestCities: async (_, { query }) => {
+      return await suggestCities(query);
+    },
   },
 };
